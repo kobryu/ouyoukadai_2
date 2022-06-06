510955667c708011e0acfd2c845ce8529b07cccb
EC2 Default User <ec2-user@ip-172-31-93-44.ec2.internal>
2022-06-06 00:29:11 +0000

    first commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,31 @@
+class ApplicationController < ActionController::Base
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!, except: [:top, :about ]
+
+  def after_sign_in_path_for(resource)
+    flash[:notice] = "Signed in successfully."
+    user_path(resource)
+
+
+  end
+
+  # def after_sign_up_path_for(resource)
+  #   flash[:notice] = "Welcome! You have signed up successfully."
+  # #   user_path(resource)
+  # end
+
+
+
+  def after_sign_out_path_for(resource)
+   flash[:notice] = "Signed out successfully."
+    root_path
+  end
+
+
+  private
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up,keys:[:email]) # 注目
+    end
+end
