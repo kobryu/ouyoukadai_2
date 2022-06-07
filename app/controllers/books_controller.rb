025126d69d9d4fec81fcec66160485a0a3394131
EC2 Default User <ec2-user@ip-172-31-93-44.ec2.internal>
2022-06-07 08:38:36 +0000

    third commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,11 +5,13 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     @book_new = Book.new
     @user = @book.user
+    @post_comment = PostComment.new
   end
 
   def index
     @book = Book.new
     @books = Book.all
+
   end
 
   def create
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,18 @@
 Rails.application.routes.draw do
+  get 'post_comments/create'
+  get 'post_comments/destroy'
+  get 'favorites/create'
+  get 'favorites/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root :to =>"homes#top"
   devise_for :users
-  resources :books, only: [:index,:show,:edit,:create,:destroy,:update]
+  resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
+    resource :favorites, only: [:create, :destroy]
+    resources :post_comments, only: [:create, :destroy]
+  end
+
+
+
   resources :users, only: [:index,:show,:edit,:update]
 
   get "home/about"=>"homes#about"
