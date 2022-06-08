826d906c0d2cf48a2a7f9e2bfceee15d9df2a90d
EC2 Default User <ec2-user@ip-172-31-93-44.ec2.internal>
2022-06-08 11:42:53 +0000

    6

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,17 +1,16 @@
 class PostCommentsController < ApplicationController
   before_action :ensure_correct_user, only: [:destroy]
   def create
-    book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
     comment = PostComment.new(post_comment_params)
     comment.user_id = current_user.id
-    comment.book_id = book.id
+    comment.book_id = @book.id
     comment.save
-    redirect_to book_path(book)
   end
 
   def destroy
+    @book = Book.find(params[:book_id])
     PostComment.find(params[:id]).destroy
-    redirect_to book_path(params[:book_id])
   end
 
   def ensure_correct_user
